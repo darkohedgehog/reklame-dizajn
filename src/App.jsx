163b869be8066443e3b3e1d51007d2973f155a99
darkohedgehog <zivic.darko79@gmail.com>
2023-07-28 21:54:44 +0200

    Adding basename in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Services from "./pages/services/Services";
@@ -13,7 +13,7 @@ import ParticlesBg from 'particles-bg';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/reklame-dizajn">
       <Navbar />
       <Routes>
         <Route index element={<Home />} />
